04fb2d1f535a91e13b32c04cad5f6da1ccde68d7
123456ab-cd <2903924884@qq.com>
2026-04-14 14:57:33 +0800

    Delete Untitled1.m

--- Untitled1.m
+++
@@ -1,9 +0,0 @@
-f = @(x) x.^4;
-I_exact = 2/5;
-fprintf('精确值: %.10f\n', I_exact);
-fprintf('-----------------------------------\n');
-for n = 1:3
-    I_approx = gauss_legendre(f, n, -1, 1);
-    err = abs(I_approx - I_exact);
-    fprintf('n=%d, 计算值=%.10f, 绝对误差=%.2e\n', n, I_approx, err);
-end
